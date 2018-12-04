205a624246ce14895bcdb8a7601225628a831764
liyuxiaoboy <liyuxiaoboy@163.com>
2018-12-04 20:15:06 +0800

    0.1

--- ioPose.py
+++ ioPose.py
@@ -5,8 +5,15 @@ import codecs
 if __name__ == '__main__':
     reader = codecs.open('img_timestamps.txt','r',encoding='utf-8')
     writer = codecs.open('rgb_qi.txt','w',encoding='utf-8')
-    for line in reader:
-        text = line.strip().split()
-        word = text[1]
-        writer.write(word+' '+'rgb/'+word+'.png')
-        writer.write('\n')
+    # for line in reader:
+    #     text = line.strip().split()
+    #     word = text[1]
+    #     writer.write(word+' '+'rgb/'+word+'.png')
+    #     writer.write('\n')
+    while lines:
+    	lines = reader.readlines(38)
+    	if not lines:
+        	break
+    	for line in lines:
+        	print(line)
+		
