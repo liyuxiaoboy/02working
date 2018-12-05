a4702f8be0ba4b358109548ed969d06b89aa8bcb
liyuxiaoboy <liyuxiaoboy@163.com>
2018-12-05 15:29:49 +0800

    have a bug

--- ioPose.py
+++ ioPose.py
@@ -1,19 +1,43 @@
 # -*-coding:utf-8 -*-
 
 import codecs
+import re
 
 if __name__ == '__main__':
-    reader = codecs.open('img_timestamps.txt','r',encoding='utf-8')
+    reader = codecs.open('corrected_poses.txt','r',encoding='utf-8')
     writer = codecs.open('rgb_qi.txt','w',encoding='utf-8')
     # for line in reader:
     #     text = line.strip().split()
     #     word = text[1]
     #     writer.write(word+' '+'rgb/'+word+'.png')
     #     writer.write('\n')
-    while lines:
+ 
+    while 1:
     	lines = reader.readlines(38)
     	if not lines:
-        	break
-    	for line in lines:
-        	print(line)
-		
+    		break
+    	for i in range(12):
+    		line=lines[i]
+        	if re.match('pcd_filename',line.strip()):
+        		pcd_filename = line.strip().split('"')
+        		word = pcd_filename[1]
+        		writer.write(word+' ')
+        	if re.match('timestamp',line.strip()):
+        		timestamp = line.strip().split()
+        		word = timestamp[1]
+        		writer.write(word+' ')
+        	if re.match('x:',line.strip()):
+        		x_ = line.strip().split()
+        		word = x_[1]
+        		writer.write(word+' ')
+        	if re.match('y:',line.strip()):
+        		y_ = line.strip().split()
+        		word = y_[1]
+        		writer.write(word+' ')
+        	if re.match('z:',line.strip()):
+        		z_ = line.strip().split()
+        		word = z_[1]
+        		writer.write(word+'\n')
+        	else:
+        		continue
+        
